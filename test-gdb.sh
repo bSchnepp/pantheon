4e29b50283c14746f6e0bcf404c4f36de13a064e
Brian Schnepp <bschneppdev@gmail.com>
2021-05-12 06:52:15 -0600

    Add definition for SD card for test scripts

--- test-gdb.sh
+++ test-gdb.sh
@@ -5,5 +5,6 @@ mkdir build
 cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=../cmake/qemu-aarch64.cmake -DCMAKE_C_FLAGS="-g3 -O0" -DCMAKE_CXX_FLAGS="-g3 -O0"
 make -j`nproc`
-qemu-system-aarch64 -machine virt,gic-version=2 -smp 8 -cpu cortex-a72 -kernel ./kernel.img -s -S -d int
+qemu-img create -fqcow2 emmc.img 4G
+qemu-system-aarch64 -machine virt,gic-version=2 -smp 8 -cpu cortex-a72 -kernel ./kernel.img  -device sdhci-pci -device sd-card,drive=flash -drive id=flash,if=none,format=qcow2,file=emmc.img -s -S -d int
 
--- test.sh
+++ test.sh
@@ -5,5 +5,6 @@ mkdir build
 cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=../cmake/qemu-aarch64.cmake
 make -j`nproc`
-qemu-system-aarch64 -machine virt,gic-version=2 -smp 8 -cpu cortex-a72 -kernel ./kernel.img -d int
+qemu-img create -fqcow2 emmc.img 4G
+qemu-system-aarch64 -machine virt,gic-version=2 -smp 8 -cpu cortex-a72 -kernel ./kernel.img -device sdhci-pci -device sd-card,drive=flash -drive id=flash,if=none,format=qcow2,file=emmc.img -d int
 
