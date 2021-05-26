f2da5aa42cdc48cef26593c56115b342ee73d596
Brian Schnepp <bschneppdev@gmail.com>
2021-05-26 15:29:57 -0600

    Initial declaration for page entry

---
+++ arch/aarch64/mmu.hpp
@@ -0,0 +1,36 @@
+#include <kern_datatypes.hpp>
+
+#ifndef _AARCH64_MMU_HPP_
+#define _AARCH64_MMU_HPP_
+
+namespace pantheon::arm
+{
+
+typedef struct PageEntry
+{
+	union
+	{
+		UINT64 Raw;
+		struct
+		{
+			UINT8 RESERVED3 : 5;
+			UINT8 SW : 4;
+			BOOL UXN : 1;
+			BOOL PXN : 1;
+			UINT64 OutputAddr : 42;
+			BOOL AF : 1;
+			UINT8 SH : 2;
+			BOOL AP : 2;
+			BOOL NS : 1;
+			UINT8 INDX : 3;
+			BOOL TB : 1;
+			BOOL VB : 1;
+		};
+		
+	};
+}__attribute__((packed)) PageEntry;
+
+}
+
+
+#endif
