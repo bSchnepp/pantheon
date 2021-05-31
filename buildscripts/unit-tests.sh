d60b13b519646b9c845b929324dc3db2149bddba
Brian Schnepp <bschneppdev@gmail.com>
2021-05-31 13:13:39 -0600

    Integration for code coverage

--- buildscripts/unit-tests.sh
+++ buildscripts/unit-tests.sh
@@ -3,6 +3,9 @@
 rm -rf build
 mkdir build
 cd build
-cmake .. -DONLY_TESTS=ON
+cmake .. -DONLY_TESTS=ON -DCMAKE_C_FLAGS="-fprofile-arcs -ftest-coverage -O2" -DCMAKE_CXX_FLAGS="-fprofile-arcs -ftest-coverage -O2"
 make -j`nproc`
-./pantheon
+./pantheon --gtest_output=xml:./gtests.xml
+cd ..
+gcovr -r . --xml-pretty > build/coverage.xml
+sonar-scanner
