130659fb884c2a18fff8977162bf4307f7a56feb
Brian Schnepp <bschneppdev@gmail.com>
2021-06-25 19:21:34 -0600

    Enable secure extensions in qemu testing environment

--- buildscripts/test-nosd.sh
+++ buildscripts/test-nosd.sh
@@ -5,5 +5,5 @@ mkdir build
 cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=../cmake/qemu-aarch64.cmake -DCMAKE_CXX_FLAGS="-O2"
 make -j`nproc`
-qemu-system-aarch64 -machine virt,gic-version=2 -smp 8 -cpu cortex-a72 -kernel ./kernel.img -d int
+qemu-system-aarch64 -machine virt,gic-version=2,secure=on -smp 8 -cpu cortex-a72 -kernel ./kernel.img -d int
 
