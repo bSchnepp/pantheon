eb94cda8fb10d4cecfa3954e23d5802c052e1da1
Brian Schnepp <bschneppdev@gmail.com>
2021-07-17 18:55:42 -0600

    Reorganize boot process to more clearly separate from the kernel proper

---
+++ Boot/Boot.cpp
@@ -0,0 +1,97 @@
+#include <arch.hpp>
+#include <kern_status.hpp>
+#include <kern_runtime.hpp>
+#include <kern_integers.hpp>
+#include <kern_datatypes.hpp>
+
+#include <Proc/kern_cpu.hpp>
+#include <Devices/kern_drivers.hpp>
+#include <PhyProtocol/DeviceTree/DeviceTree.hpp>
+
+#include <System/Memory/kern_physpaging.hpp>
+
+void Initialize(fdt_header *dtb)
+{
+	volatile bool CheckMe = CheckHeader(dtb);
+	if (!CheckMe)
+	{
+		/* Loop forever: can't really do anything. */
+		for (;;) {}
+	}
+
+	DeviceTreeBlob DTBState(dtb);
+	CHAR CurDevNode[512];
+	ClearBuffer(CurDevNode, 512);
+
+	while (!DTBState.EndStruct())
+	{
+		DTBState.NextStruct();
+		FDTNodeType CurNode = DTBState.GetStructType();
+		if (CurNode == FDT_PROP)
+		{
+			UINT64 Offset = DTBState.GetPropStructNameIndex();
+			CHAR Buffer[512];
+			ClearBuffer(Buffer, 512);
+			DTBState.CopyStringFromOffset(Offset, Buffer, 512);
+			SERIAL_LOG("%s%s%s", CurDevNode, " : ", Buffer);
+			if (IsStringPropType(Buffer) || IsStringListPropType(Buffer))
+			{
+				CHAR Buffer2[512];
+				ClearBuffer(Buffer2, 512);
+				DTBState.CopyStringFromStructPropNode(Buffer2, 512);
+				SERIAL_LOG("%s%s%s", " (", Buffer2, ")");
+			}
+			else if (IsU32PropType(Buffer))
+			{
+				UINT32 U32;
+				DTBState.CopyU32FromStructPropNode(&U32);
+				SERIAL_LOG("%s%u%s", " (", U32, ")");
+			}
+			else if (IsU64PropType(Buffer))
+			{
+				UINT64 U64;
+				DTBState.CopyU64FromStructPropNode(&U64);
+				SERIAL_LOG("%s%u%s", " (", U64, ")");
+			}
+			
+			CHAR DevName[512];
+			UINT64 Addr;
+			DTBState.NodeNameToAddress(CurDevNode, DevName, 512, &Addr);
+			DriverHandleDTB(DevName, &DTBState);
+			SERIAL_LOG("%s", "\n");
+		}
+		else if (CurNode == FDT_BEGIN_NODE)
+		{
+			ClearBuffer(CurDevNode, 512);
+			DTBState.CopyStringFromStructBeginNode(CurDevNode, 512);
+			SERIAL_LOG("%s%s%s", "<<", CurDevNode, ">>\n");
+
+			CHAR DevName[512];
+			UINT64 Addr;
+			DTBState.NodeNameToAddress(CurDevNode, DevName, 512, &Addr);
+			InitDriver(DevName, Addr);
+
+		}
+		else if (CurNode == FDT_END_NODE)
+		{
+			CHAR DevName[512];
+			UINT64 Addr;
+			DTBState.NodeNameToAddress(CurDevNode, DevName, 512, &Addr);
+			FiniDriver(DevName, Addr);
+		}
+	}
+	SERIAL_LOG("%s\n", "finished going through dtb");
+}
+
+extern "C" void BootInit(fdt_header *dtb, void *initial_load_addr, void *virt_load_addr)
+{
+	PANTHEON_UNUSED(initial_load_addr);
+	PANTHEON_UNUSED(virt_load_addr);
+
+	pantheon::CPU::CLI();
+	if (pantheon::CPU::GetProcessorNumber() == 0)
+	{
+		BoardInit();
+		Initialize(dtb);
+	}
+}
--- kern_main.cpp
+++ kern_main.cpp
@@ -6,83 +6,6 @@
 
 #include <Proc/kern_cpu.hpp>
 #include <Devices/kern_drivers.hpp>
-#include <PhyProtocol/DeviceTree/DeviceTree.hpp>
-
-#include <System/Memory/kern_physpaging.hpp>
-
-void Initialize(fdt_header *dtb)
-{
-	volatile bool CheckMe = CheckHeader(dtb);
-	if (!CheckMe)
-	{
-		/* Loop forever: can't really do anything. */
-		for (;;) {}
-	}
-
-	DeviceTreeBlob DTBState(dtb);
-	CHAR CurDevNode[512];
-	ClearBuffer(CurDevNode, 512);
-
-	while (!DTBState.EndStruct())
-	{
-		DTBState.NextStruct();
-		FDTNodeType CurNode = DTBState.GetStructType();
-		if (CurNode == FDT_PROP)
-		{
-			UINT64 Offset = DTBState.GetPropStructNameIndex();
-			CHAR Buffer[512];
-			ClearBuffer(Buffer, 512);
-			DTBState.CopyStringFromOffset(Offset, Buffer, 512);
-			SERIAL_LOG("%s%s%s", CurDevNode, " : ", Buffer);
-			if (IsStringPropType(Buffer) || IsStringListPropType(Buffer))
-			{
-				CHAR Buffer2[512];
-				ClearBuffer(Buffer2, 512);
-				DTBState.CopyStringFromStructPropNode(Buffer2, 512);
-				SERIAL_LOG("%s%s%s", " (", Buffer2, ")");
-			}
-			else if (IsU32PropType(Buffer))
-			{
-				UINT32 U32;
-				DTBState.CopyU32FromStructPropNode(&U32);
-				SERIAL_LOG("%s%u%s", " (", U32, ")");
-			}
-			else if (IsU64PropType(Buffer))
-			{
-				UINT64 U64;
-				DTBState.CopyU64FromStructPropNode(&U64);
-				SERIAL_LOG("%s%u%s", " (", U64, ")");
-			}
-			
-			CHAR DevName[512];
-			UINT64 Addr;
-			DTBState.NodeNameToAddress(CurDevNode, DevName, 512, &Addr);
-			DriverHandleDTB(DevName, &DTBState);
-			SERIAL_LOG("%s", "\n");
-		}
-		else if (CurNode == FDT_BEGIN_NODE)
-		{
-			ClearBuffer(CurDevNode, 512);
-			DTBState.CopyStringFromStructBeginNode(CurDevNode, 512);
-			SERIAL_LOG("%s%s%s", "<<", CurDevNode, ">>\n");
-
-			CHAR DevName[512];
-			UINT64 Addr;
-			DTBState.NodeNameToAddress(CurDevNode, DevName, 512, &Addr);
-			InitDriver(DevName, Addr);
-
-		}
-		else if (CurNode == FDT_END_NODE)
-		{
-			CHAR DevName[512];
-			UINT64 Addr;
-			DTBState.NodeNameToAddress(CurDevNode, DevName, 512, &Addr);
-			FiniDriver(DevName, Addr);
-		}
-	}
-	SERIAL_LOG("%s\n", "finished going through dtb");
-}
-
 
 extern "C" void svc_LogText(const CHAR *Content);
 void user_idle()
@@ -158,38 +81,14 @@ void kern_init_core()
 	}
 }
 
-#ifndef ONLY_TESTS
-extern "C" UINT64 kern_begin;
-extern "C" UINT64 kern_end;
-
-void init_pmm()
+void kern_init(void *initial_load_addr, void *virt_load_addr)
 {
-
-}
-#else
-void init_pmm()
-{
-
-}
-#endif
-
-void kern_init(fdt_header *dtb, void *initial_load_addr)
-{
+	PANTHEON_UNUSED(virt_load_addr);
 	PANTHEON_UNUSED(initial_load_addr);
 	
-	pantheon::CPU::CLI();
 	if (pantheon::CPU::GetProcessorNumber() == 0)
 	{
 		pantheon::SetKernelStatus(pantheon::KERNEL_STATUS_INIT);
-
-		/* The most basic kernel initialization should be done here. */
-		pantheon::InitGlobalPhyPageManagers();
-
-		BoardInit();
-		Initialize(dtb);
-
-		init_pmm();
-
 		pantheon::GetGlobalScheduler()->Init();
 		pantheon::SetKernelStatus(pantheon::KERNEL_STATUS_SECOND_STAGE);
 
