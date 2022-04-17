d9e29f9620c8df5ed29c6465af08c481d6b2dd1f
Brian Schnepp <bschneppdev@gmail.com>
2022-04-17 02:20:04 -0600

    Initial raspi4 build script

--- BoardSupport/raspi4/BoardRuntime/BoardRT.cpp
+++ BoardSupport/raspi4/BoardRuntime/BoardRT.cpp
@@ -14,6 +14,7 @@
 
 void WriteSerialChar(CHAR Char)
 {
+	PANTHEON_UNUSED(Char);
 	/* FIXME: Use the miniUART! */
 }
 
---
+++ cmake/raspi4.cmake
@@ -0,0 +1,39 @@
+# Assume aarch64 and not EFI for now.
+
+SET(CMAKE_SYSTEM_NAME Generic)
+SET(CMAKE_SYSTEM_PROCESSOR aarch64)
+SET(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+SET(CMAKE_C_COMPILER "clang")
+SET(CMAKE_CXX_COMPILER "clang++")
+SET(CMAKE_ASM_COMPILER "aarch64-none-elf-as")
+SET(CMAKE_LINKER "ld.lld")
+SET(CMAKE_OBJCOPY "aarch64-none-elf-objcopy")
+SET(LINKER_SCRIPT ${CMAKE_SOURCE_DIR}/BoardSupport/raspi4/linkin.ld)
+SET(CMAKE_ASM_NASM_OBJECT_FORMAT "elf64")
+SET(CMAKE_C_FLAGS "-target aarch64-none-elf -mgeneral-regs-only -mcmodel=large -ffreestanding -nostdlib -c -Wall -Wextra -Wvla -mcpu=cortex-a72 -mtune=cortex-a72 -fno-builtin -fstack-protector ${CMAKE_C_FLAGS}")
+SET(CMAKE_C_LINK_EXECUTABLE "<CMAKE_LINKER> -m aarch64elf -nostdlib <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
+SET(CMAKE_CXX_FLAGS "-target aarch64-none-elf -mcmodel=large -mgeneral-regs-only -ffreestanding -nostdlib -c -Wall -Wextra -Wvla -fno-rtti -fno-exceptions -mcpu=cortex-a72 -mtune=cortex-a72 -fno-builtin -fstack-protector ${CMAKE_CXX_FLAGS}")
+SET(CMAKE_CXX_LINK_EXECUTABLE "<CMAKE_LINKER> -m aarch64elf -nostdlib <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
+
+SET(CMAKE_EXE_LINKER_FLAGS "-nostdlib -nostartfiles")
+
+SET(FSANITIZE_FLAGS "-fsanitize=undefined")
+
+
+SET(FINAL_CMAKE_C_LINK_EXECUTABLE "<CMAKE_LINKER> -m aarch64elf -nostdlib -T ${LINKER_SCRIPT} <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
+SET(FINAL_CMAKE_CXX_LINK_EXECUTABLE "<CMAKE_LINKER> -m aarch64elf -nostdlib -T ${LINKER_SCRIPT} <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
+
+SET(TARGET_SYSTEM "raspi4")
+SET(TARGET_SYSTEM_RASPI4 ON)
+SET(TARGET_SYSTEM_BOARD "BoardSupport/raspi4")
+
+# Be sure about the name...
+SET(TARGET_PROCESSOR "aarch64")
+SET(TARGET_PROCESSOR_AARCH64 ON)
+
+# Don't even try with host stuff.
+SET(CMAKE_SHARED_LIBRARY_PREFIX "")
+SET(CMAKE_SHARED_MODULE_PREFIX "")
+
+
