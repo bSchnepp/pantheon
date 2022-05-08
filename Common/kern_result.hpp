43afc9222676b559a6b686dcd29b45b4ee2eaa4f
Brian Schnepp <bschneppdev@gmail.com>
2022-05-07 18:07:36 -0600

    Update Result to be split into an 8-bit and 24-bit region

--- Common/kern_result.hpp
+++ Common/kern_result.hpp
@@ -6,12 +6,19 @@
 namespace pantheon
 {
 
+enum ResultClass
+{
+	CLASS_NONE = (0ULL << 24),
+	CLASS_KERN = (1ULL << 24),
+};
+
 enum class Result : INT32
 {
-	SYS_CONN_CLOSED = -3,
-	SYS_OOM = -2,
-	SYS_FAIL = -1,
-	SYS_OK = 0,
+	SYS_CONN_CLOSED = 3 | CLASS_KERN,
+	SYS_OOM = 2 | CLASS_KERN,
+	SYS_FAIL = 1 | CLASS_KERN,
+
+	SYS_OK = 0 | CLASS_NONE,
 };
 
 }
